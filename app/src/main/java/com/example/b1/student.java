846dd9737628d6664b345563e7d2a19b96e536f8
theu-nt <nguyentheu191099@gmail.com>
2021-08-19 22:15:53 +0700

    ha

--- app/src/main/java/com/example/b1/student.java
+++ app/src/main/java/com/example/b1/student.java
@@ -1,4 +1,21 @@
 package com.example.b1;
 
 public class student {
+    String name,id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
